329f10d0d79fe675d0d099d0e2fe6f7e4f77dea4
Leonardo Lacerda <leonardo.natercio@gmail.com>
2019-12-03 02:46:24 -0300

    server js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,30 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// const forceSSL = function() {
-//   return function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https') {
-//       return res.redirect(
-//        ['https://', req.get('Host'), req.url].join('')
-//       );
-//     }
-//     next();
-//   }
-// }
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 
-// app.use(forceSSL());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-test'));
 
+const path = require('path');
+
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular-test/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/angular-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
